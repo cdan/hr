6cfa68e84d89fe7389ae660ab8cb269ddca617bf
unknown <cdan@.(none)>
2011-02-15 11:25:00 +0800

    fix alert

--- src/hot/org/xianairlines/action/staffs/NewHireAlertList.java
+++ src/hot/org/xianairlines/action/staffs/NewHireAlertList.java
@@ -1,43 +1,16 @@
 package org.xianairlines.action.staffs;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.framework.EntityQuery;
+import org.xianairlines.model.Staffs;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import net.sf.jxls.transformer.XLSTransformer;
-
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Out;
-import org.jboss.seam.framework.EntityQuery;
-import org.xianairlines.model.Staffs;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 
 //入职提醒列表
@@ -57,15 +30,12 @@ public class NewHireAlertList extends EntityQuery<Staffs> {
 	@Override
     public List getResultList() {
 		Calendar c = Calendar.getInstance();
-		Calendar sixmonth = (Calendar)c.clone();
-		sixmonth.add(Calendar.MONTH, -6);
-		Calendar fivemonth = (Calendar)c.clone();
-		fivemonth.add(Calendar.MONTH, -5);
+		Calendar onemonth = (Calendar)c.clone();
+		onemonth.add(Calendar.MONTH, +1);
 		
 		List<Object[]> result =
-		      entityManager.createQuery("select staffs from Staffs staffs where staffs.startDate >= :sixmonth and staffs.startDate <= :fivemonth")
-		      .setParameter("sixmonth", sixmonth.getTime())
-		      .setParameter("fivemonth", fivemonth.getTime())
+		      entityManager.createQuery("select staffs from Staffs staffs where staffs.zhuanzhengdate <= :onemonth")
+		      .setParameter("onemonth", onemonth.getTime())
 		      .getResultList();		
 		return result;
 	}
